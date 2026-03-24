97dd3cdc6427e04b12f710562ba55fa0e42362f8
zakari2-ben <riariazakaria6@gmail.com>
2026-03-25 00:37:33 +0100

    add getters for json respones

--- backend/app/Http/Resources/UserResource.php
+++ backend/app/Http/Resources/UserResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Storage;
 
 class UserResource extends JsonResource
 {
@@ -15,11 +16,16 @@ class UserResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id' => $this->id,
-            'name' => $this->name,
-            'email' => $this->email,
-            'created_at' => $this->created_at,
-            'updated_at' => $this->updated_at,
+            'id'                => $this->id,
+            'name'              => $this->name,
+            'email'             => $this->email,
+            'email_verified_at' => $this->email_verified_at,
+            'email_verified'    => $this->hasVerifiedEmail(),
+            'avatar_url'        => $this->avatar_path
+                ? Storage::disk('public')->url($this->avatar_path)
+                : null,
+            'created_at'        => $this->created_at,
+            'updated_at'        => $this->updated_at,
         ];
     }
 }
